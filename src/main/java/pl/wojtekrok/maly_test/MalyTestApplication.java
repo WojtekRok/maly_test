d0419fd6e475c62b04ec2671f16ae4a18c9ff842
WojtekRok <57679791+WojtekRok@users.noreply.github.com>
2020-04-29 12:35:11 +0200

    first commit

---
+++ src/main/java/pl/wojtekrok/maly_test/MalyTestApplication.java
@@ -0,0 +1,13 @@
+package pl.wojtekrok.maly_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MalyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/wojtekrok/maly_test/MalyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.wojtekrok.maly_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
